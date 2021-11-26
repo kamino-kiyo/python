253e2bcd8b472e35f4058a55095c999b14cb21d5
kamino.kiyo <kamino.kiyo@furyu.jp>
2021-11-26 17:57:08 +0900

    trimをpyファイルに変換

--- ThemeGuesser/app.py
+++ ThemeGuesser/app.py
@@ -1,9 +1,15 @@
+import sys
 from flask import Flask
+
+
+sys.path.append('ThemeGuesser')
+import trim
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello'
+    return 'trim!'
 
 app.run('0.0.0.0', port=5000, debug=True)
